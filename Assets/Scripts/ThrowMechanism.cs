638e1f2bfd1fa8299e6a48655b3c1c6f8e3cfb82
ויויאן אומנסקי <vivien.umansky@msmail.ariel.ac.il>
2024-12-10 22:37:04 +0200

    update

--- Assets/Scripts/ThrowMechanism.cs
+++ Assets/Scripts/ThrowMechanism.cs
@@ -3,5 +3,4 @@ using UnityEngine;
 public class ThrowMechanism : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D arm; // Rigidbody of the throwing arm
-    [SerializeField] private float torqueForce = 500f; // Amount of rotational force
 }
